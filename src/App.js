0a23ba31fddcb19ec037fba053b29b1c286893c6
allcj <allcj@cin.ufpe.br>
2017-09-24 15:22:05 -0300

    gpio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import gpio from 'gpio';
 
+var gpio22 = gpio.export(22, {direction: "out"});
+
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
